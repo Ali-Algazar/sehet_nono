08b2f4319eb90400ac93914abdcd44c11f695112
Ali Mohamed <ilamohemed12@gmail.com>
2025-10-28 20:29:53 +0300

    Implement AuthCubit and AuthState for user authentication management

---
+++ lib/features/auth/presentation/cubit/auth_cubit.dart
@@ -0,0 +1,42 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:sehet_nono/features/auth/data/repositories/auth_repository.dart';
+
+part 'auth_state.dart';
+
+class AuthCubit extends Cubit<AuthState> {
+  AuthCubit({required this.authRepository}) : super(AuthInitial());
+  AuthRepository authRepository;
+  Future<void> login({required String email, required String password}) async {
+    emit(AuthLoading());
+    final result = await authRepository.login(email: email, password: password);
+    result.fold(
+      (failure) => emit(AuthError(failure.message)),
+      (user) => emit(AuthSuccess()),
+    );
+  }
+
+  Future<void> register({
+    required String email,
+    required String password,
+  }) async {
+    emit(AuthLoading());
+    final result = await authRepository.register(
+      email: email,
+      password: password,
+    );
+    result.fold(
+      (failure) => emit(AuthError(failure.message)),
+      (user) => emit(AuthSuccess()),
+    );
+  }
+
+  Future<void> logout() async {
+    emit(AuthLoading());
+    final result = await authRepository.logout();
+    result.fold(
+      (failure) => emit(AuthError(failure.message)),
+      (user) => emit(AuthSuccess()),
+    );
+  }
+}
