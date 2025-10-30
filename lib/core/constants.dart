d4bac13a0d26b5d3d7bfaf206c08bd73e3ca73dc
Ali Mohamed <ilamohemed12@gmail.com>
2025-10-30 20:04:11 +0300

    Add kPendingOperationsKey constant for pending operations management

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -3,3 +3,4 @@ const String kChildrenBox = 'childrenBox';
 const String userKey = 'user';
 const String kAuthTokenKey = 'auth_token';
 const String kIsOnboardingShowKey = 'show_onboarding';
+const String kPendingOperationsKey = 'pending_operations';
