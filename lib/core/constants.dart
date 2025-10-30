4110dc91bea4fbbe527d2af3664319d229465fa8
Ali Mohamed <ilamohemed12@gmail.com>
2025-10-30 16:23:06 +0300

    Add kChildrenBox constant for child data storage

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,4 +1,5 @@
 const String userBox = 'userBox';
+const String kChildrenBox = 'childrenBox';
 const String userKey = 'user';
 const String kAuthTokenKey = 'auth_token';
 const String kIsOnboardingShowKey = 'show_onboarding';
