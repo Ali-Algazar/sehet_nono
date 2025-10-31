d38bb7abb1d595c34ea6b0899833bab6f7610dd6
Ali Mohamed <ilamohemed12@gmail.com>
2025-10-31 10:58:18 +0200

    Refactor ChildrenRepository methods to return List<ChildModel> instead of void

--- lib/features/children/data/repositories/children_repository.dart
+++ lib/features/children/data/repositories/children_repository.dart
@@ -3,13 +3,19 @@ import 'package:sehet_nono/core/errors/failures.dart';
 import 'package:sehet_nono/features/children/data/model/child_model.dart';
 
 abstract class ChildrenRepository {
-  Future<Either<String, void>> addChild(
+  Future<Either<String, List<ChildModel>>> addChild(
     String name,
     String birthDate,
     String gender, {
     isSync = false,
   });
   Future<Either<Failure, List<ChildModel>>> getChildren();
-  Future<Either<String, void>> updateChild(ChildModel child, {isSync = false});
-  Future<Either<String, void>> deleteChild(String childId, {isSync = false});
+  Future<Either<String, List<ChildModel>>> updateChild(
+    ChildModel child, {
+    isSync = false,
+  });
+  Future<Either<String, List<ChildModel>>> deleteChild(
+    String childId, {
+    isSync = false,
+  });
 }
--- lib/features/children/data/repositories/children_repository_impl.dart
+++ lib/features/children/data/repositories/children_repository_impl.dart
@@ -22,7 +22,7 @@ class ChildrenRepositoryImpl implements ChildrenRepository {
   });
 
   @override
-  Future<Either<String, void>> deleteChild(
+  Future<Either<String, List<ChildModel>>> deleteChild(
     String childId, {
     isSync = false,
   }) async {
@@ -35,7 +35,10 @@ class ChildrenRepositoryImpl implements ChildrenRepository {
           var response = await remoteDataSource.deleteChild(childId);
           if (response.statusCode == 200) {
             await localDataSource.clearChild(childId);
-            return Right(null);
+
+            List<ChildModel> children =
+                await localDataSource.clearChildrenList() as List<ChildModel>;
+            return Right(children);
           } else {
             return Left(
               '${response.data['message']} with status code ${response.statusCode}',
@@ -54,11 +57,17 @@ class ChildrenRepositoryImpl implements ChildrenRepository {
               createdAt: DateTime.now(),
             ),
           );
-          return right(null);
+          List<ChildModel> children =
+              await localDataSource.clearChildrenList() as List<ChildModel>;
+
+          return right(children);
         }
       } else {
         await remoteDataSource.deleteChild(childId);
-        return Right(null);
+        List<ChildModel> children =
+            await localDataSource.clearChildrenList() as List<ChildModel>;
+
+        return Right(children);
       }
     } on Exception catch (e) {
       return left(e.toString());
@@ -93,7 +102,7 @@ class ChildrenRepositoryImpl implements ChildrenRepository {
   }
 
   @override
-  Future<Either<String, void>> updateChild(
+  Future<Either<String, List<ChildModel>>> updateChild(
     ChildModel child, {
     isSync = false,
   }) async {
@@ -107,7 +116,10 @@ class ChildrenRepositoryImpl implements ChildrenRepository {
           if (remoteChild.statusCode == 200) {
             var child1 = await localDataSource.getCachedChild(child.id);
             child1?.save();
-            return Right(null);
+            List<ChildModel> children =
+                await localDataSource.clearChildrenList() as List<ChildModel>;
+
+            return Right(children);
           } else {
             return Left(
               '${remoteChild.data['message']} with status code ${remoteChild.statusCode}',
@@ -126,11 +138,17 @@ class ChildrenRepositoryImpl implements ChildrenRepository {
               createdAt: DateTime.now(),
             ),
           );
-          return right(null);
+          List<ChildModel> children =
+              await localDataSource.clearChildrenList() as List<ChildModel>;
+
+          return right(children);
         }
       } else {
         await remoteDataSource.updateChild(child);
-        return right(null);
+        List<ChildModel> children =
+            await localDataSource.clearChildrenList() as List<ChildModel>;
+
+        return right(children);
       }
     } catch (e) {
       return left(e.toString());
@@ -138,18 +156,18 @@ class ChildrenRepositoryImpl implements ChildrenRepository {
   }
 
   @override
-  Future<Either<String, void>> addChild(
+  Future<Either<String, List<ChildModel>>> addChild(
     String name,
     String birthDate,
     String gender, {
     isSync = false,
   }) async {
-    if (isSync == false) {
-      final results = await connectivity.checkConnectivity();
-      final hasConnection =
-          results.isNotEmpty && results.first != ConnectivityResult.none;
-      if (hasConnection) {
-        try {
+    try {
+      if (isSync == false) {
+        final results = await connectivity.checkConnectivity();
+        final hasConnection =
+            results.isNotEmpty && results.first != ConnectivityResult.none;
+        if (hasConnection) {
           final remoteChild = await remoteDataSource.addChild(
             name,
             birthDate,
@@ -158,12 +176,11 @@ class ChildrenRepositoryImpl implements ChildrenRepository {
           await localDataSource.cacheChild(
             ChildModel.fromJson(remoteChild.data),
           );
-          return Right(null);
-        } catch (e) {
-          return Left('Failed to add child remotely: $e');
-        }
-      } else {
-        try {
+          List<ChildModel> children =
+              await localDataSource.clearChildrenList() as List<ChildModel>;
+
+          return Right(children);
+        } else {
           await localDataSource.cacheChild(
             ChildModel(
               id: Uuid().v4(),
@@ -185,18 +202,20 @@ class ChildrenRepositoryImpl implements ChildrenRepository {
               createdAt: DateTime.now(),
             ).toMap(),
           );
-          return Right(null);
-        } on Exception catch (e) {
-          return Left('Failed to add child locally: $e');
+          List<ChildModel> children =
+              await localDataSource.clearChildrenList() as List<ChildModel>;
+
+          return Right(children);
         }
-      }
-    } else {
-      try {
+      } else {
         await remoteDataSource.addChild(name, birthDate, gender);
-        return right(null);
-      } catch (e) {
-        return left('Failed to sync added child: $e');
+        List<ChildModel> children =
+            await localDataSource.clearChildrenList() as List<ChildModel>;
+
+        return right(children);
       }
+    } on Exception catch (e) {
+      return left(e.toString());
     }
   }
 }
