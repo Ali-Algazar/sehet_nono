32f6840abb2ad8deeef9dafbe5ce6ba65a5e1193
Ali Mohamed <ilamohemed12@gmail.com>
2025-11-01 09:58:56 +0200

    Add ScheduleRepository interface for managing vaccine records

---
+++ lib/features/schedule/data/repositories/schedule_repository.dart
@@ -0,0 +1,15 @@
+import 'package:dartz/dartz.dart';
+import 'package:sehet_nono/core/errors/failures.dart';
+import 'package:sehet_nono/features/schedule/data/model/vaccine_record_model.dart';
+
+abstract class ScheduleRepository {
+  Future<Either<Failure, List<VaccineRecordModel>>> getVaccineRecordList(
+    String childId,
+  );
+  Future<Either<Failure, List<VaccineRecordModel>>> updateVaccineRecord(
+    String scheduleId, {
+    String? status,
+    String? dateAdministered,
+    String? notes,
+  });
+}
