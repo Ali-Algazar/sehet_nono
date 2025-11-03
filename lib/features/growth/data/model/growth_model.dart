859fc521ae6e1af2954432e4f94c9670a43d85c7
Ali Mohamed <ilamohemed12@gmail.com>
2025-11-03 08:06:33 +0200

    Enhance GrowthModel to extend HiveObject and include note field; update GrowthModelAdapter for serialization

--- lib/features/growth/data/model/growth_model.dart
+++ lib/features/growth/data/model/growth_model.dart
@@ -3,7 +3,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 part 'growth_model.g.dart';
 
 @HiveType(typeId: 4)
-class GrowthModel {
+class GrowthModel extends HiveObject {
   @HiveField(0)
   final String id;
   @HiveField(1)
@@ -17,6 +17,9 @@ class GrowthModel {
   @HiveField(5)
   final String dateMeasured;
 
+  @HiveField(6)
+  String? note;
+
   GrowthModel({
     required this.id,
     required this.childId,
@@ -24,6 +27,7 @@ class GrowthModel {
     required this.weight,
     this.headCircumference,
     required this.dateMeasured,
+    this.note,
   });
   factory GrowthModel.fromJson(Map<String, dynamic> json) {
     return GrowthModel(
@@ -33,6 +37,40 @@ class GrowthModel {
       weight: json['weight'] as String,
       headCircumference: json['headCircumference'] as String?,
       dateMeasured: json['dateMeasured'] as String,
+    );
+  }
+  Map<String, dynamic> toJson() {
+    return {
+      'height': height,
+      'weight': weight,
+      'headCircumference': headCircumference,
+      'dateMeasured': dateMeasured,
+      'note': note,
+    };
+  }
+
+  @override
+  String toString() {
+    return 'GrowthModel(id: $id, childId: $childId, height: $height, weight: $weight, headCircumference: $headCircumference, dateMeasured: $dateMeasured)';
+  }
+
+  GrowthModel copyWith({
+    String? id,
+    String? childId,
+    String? height,
+    String? weight,
+    String? headCircumference,
+    String? dateMeasured,
+    String? note,
+  }) {
+    return GrowthModel(
+      id: id ?? this.id,
+      childId: childId ?? this.childId,
+      height: height ?? this.height,
+      weight: weight ?? this.weight,
+      headCircumference: headCircumference ?? this.headCircumference,
+      dateMeasured: dateMeasured ?? this.dateMeasured,
+      note: note ?? this.note,
     );
   }
 }
--- lib/features/growth/data/model/growth_model.g.dart
+++ lib/features/growth/data/model/growth_model.g.dart
@@ -23,13 +23,14 @@ class GrowthModelAdapter extends TypeAdapter<GrowthModel> {
       weight: fields[3] as String,
       headCircumference: fields[4] as String?,
       dateMeasured: fields[5] as String,
+      note: fields[6] as String?,
     );
   }
 
   @override
   void write(BinaryWriter writer, GrowthModel obj) {
     writer
-      ..writeByte(6)
+      ..writeByte(7)
       ..writeByte(0)
       ..write(obj.id)
       ..writeByte(1)
@@ -41,7 +42,9 @@ class GrowthModelAdapter extends TypeAdapter<GrowthModel> {
       ..writeByte(4)
       ..write(obj.headCircumference)
       ..writeByte(5)
-      ..write(obj.dateMeasured);
+      ..write(obj.dateMeasured)
+      ..writeByte(6)
+      ..write(obj.note);
   }
 
   @override
