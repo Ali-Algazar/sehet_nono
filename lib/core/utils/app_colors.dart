4e759fff87a2bb31849b0b213ac6a40e9040c7ea
Ali Mohamed <ilamohemed12@gmail.com>
2025-12-15 10:45:51 +0200

    Add AppColors class with color constants and a linear gradient

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xff93D8D4);
+  static const Color bG = Color(0xffFEFEFE);
+  static const Color mText = Color(0xff333745);
+  static const Color dText = Color(0xff797F8F);
+  static const Color graphic = Color(0xffE0E0E0);
+  static const Color danger = Color(0xffC01304);
+  static const Color success = Color(0xff4F9F52);
+  static const LinearGradient primaryL = LinearGradient(
+    colors: [Color(0xff50C2C9), Color(0xffA4D4AE)],
+     begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+  );
+  // static const Color primarylineer
+}
