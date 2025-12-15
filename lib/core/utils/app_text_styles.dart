4f91283e55ec08354c28e2a03b23ab4b8af44a25
Ali Mohamed <ilamohemed12@gmail.com>
2025-12-15 10:46:06 +0200

    Add AppTextStyles class with text style constants for headers, body, buttons, and captions

---
+++ lib/core/utils/app_text_styles.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const TextStyle headerBold = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle headerRagular = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle body1Bold = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle body1Ragular = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle body2Bold = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle body2Ragular = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle btnsBold = TextStyle(
+    fontSize: 15,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle btnsRagular = TextStyle(
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle captionLight = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w300,
+  );
+  static const TextStyle captionRagular = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+  );
+}
